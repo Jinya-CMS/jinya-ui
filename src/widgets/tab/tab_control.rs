770beefcdc3ff8df6e37e64cfb9d2b2ceb475362
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2020-07-19 20:21:57 +0200

    Finalized tab control

--- src/widgets/tab/tab_control.rs
+++ src/widgets/tab/tab_control.rs
@@ -1,4 +1,3 @@
-use uuid::Uuid;
 use yew::prelude::*;
 
 pub fn get_css<'a>() -> &'a str {
@@ -50,15 +49,15 @@ pub fn get_css<'a>() -> &'a str {
 pub struct TabPage {
     pub title: String,
     pub content: Html,
-    uniq_id: Uuid,
+    pub name: String,
 }
 
 impl TabPage {
-    pub fn new(title: String, content: Html) -> Self {
+    pub fn new(title: String, content: Html, name: String) -> Self {
         TabPage {
             title,
             content,
-            uniq_id: Uuid::new_v4(),
+            name,
         }
     }
 }
@@ -66,7 +65,7 @@ impl TabPage {
 pub struct TabControl {
     link: ComponentLink<Self>,
     pages: Vec<TabPage>,
-    active_page_uuid: Uuid,
+    active_page_name: String,
 }
 
 #[derive(PartialEq, Clone, Properties)]
@@ -83,20 +82,20 @@ impl Component for TabControl {
     type Properties = TabControlProps;
 
     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        let active_page = props.pages.first().unwrap().uniq_id;
+        let active_page = props.pages.first().unwrap().name.clone();
 
         TabControl {
             link,
             pages: props.pages,
-            active_page_uuid: active_page,
+            active_page_name: active_page,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> bool {
         match msg {
             Msg::ChangeTab(idx) => {
-                let page = self.pages[idx].uniq_id;
-                self.active_page_uuid = page;
+                let page = self.pages[idx].name.clone();
+                self.active_page_name = page;
             }
         }
 
@@ -115,14 +114,14 @@ impl Component for TabControl {
                 <nav class="jinya-tab__link-list">
                     {for self.pages.iter().enumerate().map(move |(idx, item)| {
                         html! {
-                            <a class=self.get_link_class(item.uniq_id) onclick=self.link.callback(move |_| Msg::ChangeTab(idx))>{&item.title}</a>
+                            <a class=self.get_link_class(&item.name) onclick=self.link.callback(move |_| Msg::ChangeTab(idx))>{&item.title}</a>
                         }
                     })}
                 </nav>
                 <div class="jinya-tab__pages">
                     {for self.pages.iter().enumerate().map(|(_, mut item)| {
                         html! {
-                            <div class=self.get_page_class(item.uniq_id)>
+                            <div class=self.get_page_class(&item.name)>
                                 {item.content.clone()}
                             </div>
                         }
@@ -134,16 +133,16 @@ impl Component for TabControl {
 }
 
 impl TabControl {
-    fn get_page_class(&self, id: Uuid) -> String {
-        if id == self.active_page_uuid {
+    fn get_page_class(&self, name: &String) -> String {
+        if name.eq(&self.active_page_name) {
             "jinya-tab__page jinya-tab__page--active".to_string()
         } else {
             "jinya-tab__page".to_string()
         }
     }
 
-    fn get_link_class(&self, id: Uuid) -> String {
-        if id == self.active_page_uuid {
+    fn get_link_class(&self, name: &String) -> String {
+        if name.eq(&self.active_page_name) {
             "jinya-tab__link jinya-tab__link--active".to_string()
         } else {
             "jinya-tab__link".to_string()
